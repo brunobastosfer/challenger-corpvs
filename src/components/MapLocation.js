b8a1c6a18a023df18315694841e9a5961a5c437d
brunobastosfer <brunobastos@edu.unifor.br>
2021-06-23 11:09:03 -0300

    adicionando os endpoints no mapa

--- src/components/MapLocation.js
+++ src/components/MapLocation.js
@@ -11,10 +11,28 @@ export function MapLocation() {
     .then((res) => setUser(res.data))
     setLoading(false)
   }, [])
-  
+
   return (
     <div>
       <pre>{JSON.stringify(user)}</pre>
+      <MapContainer center={[-3.74, -38.56]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {
+          user.map((item) => { 
+            return (
+              <>
+          <Marker position={[item.lat, item.long]}>
+            <Popup>
+              Fioravante
+            </Popup>
+          </Marker>
+          </>
+          )})
+        }
+      </MapContainer>
     </div>
   );
 }
